178ab48eb09033e493d6173f844890b96e0d9b5b
Alex Mavrogiannis <nalfemp@gmail.com>
2013-08-29 23:22:05 +0300

    layers now working

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render_to_response
+from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 import Image, ImageDraw
 from .models import Marker
@@ -9,22 +10,22 @@ WORLD_HEIGHT=6144
 IMAGE_WIDTH=10000
 IMAGE_HEIGHT=10000
 
-
 SQSIZE=3
 LQSIZE=2
 
+@login_required
 def landing(request):
-    im = Image.open("media/map.png")
+    im = Image.open("media/trans_map.png")
     global IMAGE_WIDTH
     global IMAGE_HEIGHT
     IMAGE_WIDTH, IMAGE_HEIGHT = im.size
     draw = ImageDraw.Draw(im)
-    net = Marker.objects.all()
-    for pt in net:
-            print pt
+    markers = Marker.objects.all()
+    for pt in markers:
+            # print pt
             x,y = normalize(pt.getX(), pt.getY())
             draw.rectangle([x+2, y+2, x-2, y-2])
-    im.save("media/map.png", "PNG")
+    im.save("media/trans_map.png", "PNG")
 
     return render_to_response("landing.html", locals(), RequestContext(request))
 
