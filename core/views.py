68f6ebeaabcbbf52e1d23d767732e05d69c65780
Gregory Lyras <gregliras@gmail.com>
2013-08-30 20:17:37 +0300

    opts

--- core/views.py
+++ core/views.py
@@ -4,14 +4,16 @@ from django.template import RequestContext
 import Image, ImageDraw
 from .models import Marker
 
+import deepzoom
+
 WORLD_WIDTH=6144
 WORLD_HEIGHT=6144
 
 IMAGE_WIDTH=10000
 IMAGE_HEIGHT=10000
 
-SQSIZE=4
-LQSIZE=3
+SQSIZE=3
+LQSIZE=2
 
 def fix_data(f = "media/points.dat"):
     fp = open(f)
@@ -20,23 +22,35 @@ def fix_data(f = "media/points.dat"):
         m = Marker()
         m.x = x
         m.y = y
+        m.new = True
+        m.visits = 1
         m.save()
     fp.close()
 
 @login_required
 def landing(request):
     #fix_data()
-    im = Image.open("media/trans_empty.png")
     global IMAGE_WIDTH
     global IMAGE_HEIGHT
-    IMAGE_WIDTH, IMAGE_HEIGHT = im.size
-    draw = ImageDraw.Draw(im)
-    markers = Marker.objects.all()
-    for pt in markers:
-            print pt
-            x,y = normalize(pt.getX(), pt.getY())
-            draw.rectangle([(x-SQSIZE, y-SQSIZE), (x+SQSIZE, y+SQSIZE)], fill=(255, 255))
-    im.save("media/trans_map.png", "PNG")
+    markers = Marker.objects.filter(new = True)
+    print len(markers)
+    if len(markers) > 0:
+        im = Image.open("media/map_full.png")
+        IMAGE_WIDTH, IMAGE_HEIGHT = im.size
+        draw = ImageDraw.Draw(im)
+
+        for pt in markers:
+                #print pt
+                pt.check()
+                pt.save()
+                x,y = normalize(pt.getX(), pt.getY())
+                draw.rectangle([(x-SQSIZE, y-SQSIZE), (x+SQSIZE, y+SQSIZE)], fill="white")
+
+        im.save("media/map_full.png", "PNG")
+        dz_creator = deepzoom.ImageCreator(tile_size=128, tile_overlap=2, tile_format="png",
+                                                image_quality=0.8, resize_filter="bicubic")
+        dz_creator.create("media/map_full.png", "media/map_full.dzi")
+
 
     return render_to_response("landing.html", locals(), RequestContext(request))
 
--- regnum/settings.py
+++ regnum/settings.py
@@ -126,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'core',
+    'loupe',
 )
 
 # A sample logging configuration. The only tangible logging
